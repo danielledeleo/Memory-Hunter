ad5c48be8a91723924bc80145630705e295057d5
jagger27 <jagger@twoseven.ca>
2014-11-03 17:23:19 -0500

    Finished writing the remaining functions

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include "mhDefs.h"
 #include "stuDefs.h"
 
+void mh_freeblock(HeapType *, int);
+
 int main()
 {
   HeapType *heap;
@@ -41,12 +43,92 @@ int main()
 }
 
 void mh_init(HeapType *heap) {
-  heap->numBlocks = 0;
-  heap->blocks    = (*BlockType) calloc(MAX_BLK, sizeof(BlockType) );
+  heap->numBlocks = MAX_BLK;
+  heap->blocks = (BlockType *) calloc(MAX_BLK, sizeof(BlockType));
+
+  int i;
+  for (i = 0; i < heap->numBlocks; i++)
+  {
+    heap->blocks[i].rsv  = C_FALSE;
+    heap->blocks[i].addr = NULL;
+    heap->blocks[i].size = 0;
+    // heap->blocks[i].tag[0] = 0;
+    // heap->blocks[i].tag[MAX_STR-1] = 0;
+  }
 }
-void mh_cleanup(HeapType *heap) {}
-void *mh_alloc(HeapType *heap, int n, char *label) {}
-void mh_dealloc(HeapType *heap, void *addr) {}
-int  mh_count(HeapType *heap) {}
-void mh_dump(HeapType *heap) {}
-void mh_collect(HeapType *heap) {}
+void mh_cleanup(HeapType *heap) {
+  int i;
+  mh_collect(heap);
+  free(heap->blocks);
+}
+void *mh_alloc(HeapType *heap, int n, char *label) {
+  int i;
+  for (i = 0; i<MAX_BLK; i++) {
+    if (heap->blocks[i].rsv == C_FALSE) {
+      heap->blocks[i].rsv  = C_TRUE;
+      heap->blocks[i].addr = malloc(n);
+      heap->blocks[i].size = n;
+      strcpy(heap->blocks[i].tag, label);
+      break;
+    }
+  }
+
+  if (i == MAX_BLK) {
+    printf("Memory hunter heap space exceeded. Exiting.\n");
+    exit(EXIT_FAILURE);
+  }
+  return heap->blocks[i].addr;
+}
+void mh_dealloc(HeapType *heap, void *addr) {
+  int i;
+  for (i = 0; i < MAX_BLK; i++) 
+  {
+    if (heap->blocks[i].addr == addr) {
+      mh_freeblock(heap, i);
+      break;
+    }
+  }
+
+  if (i == MAX_BLK) {
+    printf("Address not tracked by memory hunter. Exiting.\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+int  mh_count(HeapType *heap) {
+  int sum = 0;
+  int i;
+
+  for(i = 0; i<MAX_BLK; i++) {
+    sum += heap->blocks[i].size * heap->blocks[i].rsv;
+  }
+
+  return sum;
+}
+void mh_dump(HeapType *heap) {
+  int i;
+  for(i = 0; i<MAX_BLK; i++) {
+    if(heap->blocks[i].rsv == C_TRUE) {
+      printf("%20s:%7d bytes stored at %p\n", heap->blocks[i].tag,
+                                              heap->blocks[i].size,
+                                              heap->blocks[i].addr);
+    }
+  }
+}
+void mh_collect(HeapType *heap) {
+  int i;
+  for (i = 0; i < MAX_BLK; i++) {
+    mh_freeblock(heap, i);
+  }
+}
+
+
+void mh_freeblock(HeapType *heap, int i) {
+  heap->blocks[i].rsv  = C_FALSE;
+  free(heap->blocks[i].addr);
+  heap->blocks[i].addr = NULL;
+  heap->blocks[i].size = 0;
+  strcpy(heap->blocks[i].tag, "");
+}
+
+
